5daecfd285cc43d4de47d289451b1a4c685d16dc
akilmatta <akilrinku@gmail.com>
2020-05-18 20:49:09 +0530

    config-server commit

---
+++ ConfigServer/src/test/java/com/cap/cs/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
